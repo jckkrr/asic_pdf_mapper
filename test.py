d6b73169e7d36d86a7a31a44707f8b16d346c11d
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2023-02-20 21:07:05 +1100

    Add files via upload

--- test.py
+++ test.py
@@ -1,27 +1,22 @@
 ### streamlit run "C:\Users\Jack\Documents\Python_projects\2023\asic_document_reader\test.py"
 
 import fitz
-
 import streamlit as st
 
 #############
 
-
 uploaded_file = st.file_uploader("")
 
 st.write(uploaded_file)
-st.write("Filename: ", uploaded_file.name)
-        
-with fitz.open(stream=uploaded_file.read(), filetype="pdf") as doc:       
-            
-    st.write(doc)
-        
-    for page in doc:      
-            
-        newtext = page.getText()
-            
-        st.write(newtext)
-            
+
+if uploaded_file != None:
     
-    
+    with fitz.open(stream=uploaded_file.read(), filetype="pdf") as doc:    
+        
+        filetext = ""
+        for page in doc:            
+            newtext = page.getText()
+            filetext += newtext
+            
+        st.write(filetext)
     
