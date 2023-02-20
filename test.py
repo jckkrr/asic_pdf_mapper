6a16bad96f04f48b4aec56af366e7c3fb9e4c02c
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2023-02-20 21:21:23 +1100

    Add files via upload

--- test.py
+++ test.py
@@ -9,19 +9,21 @@ uploaded_file = st.file_uploader("")
 
 st.write(uploaded_file)
 
+
+
 if uploaded_file != None:
     
-    with fitz.open(stream=uploaded_file.read(), filetype="pdf") as doc:    
+    doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
+    
+
+    
+    st.write(doc.page_count)
+    st.write(doc.name, len(doc.name))
+    
+    for page in doc:
         
-        st.write(doc.page_count)
-        
-        filetext = ""
-        for page in doc: 
-            
-            st.write(page)
-            
-            newtext = page.getText()
-            filetext += newtext
-            
-        st.write(filetext)
+        t = page.get_text()
+    
+        st.write(t)
+    
     
