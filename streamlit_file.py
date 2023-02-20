fab2305b3540a7371b50dd4a60c68fa359f37835
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2023-02-20 21:31:12 +1100

    Add files via upload

--- streamlit_file.py
+++ streamlit_file.py
@@ -64,6 +64,9 @@ if df.shape[0] > 0:
     components.html(HtmlFile.read(), height=500)
     
     downloadHTML = str(open(f'temp.html', 'r', encoding='utf-8').read()).replace('border: 1px', 'border: 0px').replace('height: 500px;', 'height: 100%')
+    
+    st.write(downloadHTML)
+    
     st.download_button(
         "Press to Download HTML",
         downloadHTML,
